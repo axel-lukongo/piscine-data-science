56554ea16165e1ccd7e89a6ce7c387247e5dc9b5
axel <lukongo_axel91100@yahoo.fr>
2023-09-26 13:45:45 +0200

    00 finish

--- data-engineer/02/table.py
+++ data-engineer/02/table.py
@@ -3,8 +3,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 import sys
 
+if len(sys.argv) < 2:
+    print(" !!! you must provide a file !!!")
+    exit()
 av = sys.argv[1]
-
 engine = create_engine('postgresql://alukongo:mysecretpassword@localhost:5433/postgres')
 
 # Répertoire contenant les fichiers CSV
@@ -13,7 +15,7 @@ directory = '/home/alukongo/Téléchargements/subject/customer'
 # Parcours des fichiers CSV dans le répertoire
 for filename in os.listdir(directory):
     if filename == av:
-      print("table en cours de creation")
+      print("table en cours de creation...")
       if filename.endswith('.csv'):
           table_name = os.path.splitext(filename)[0]
           file_path = os.path.join(directory, filename)
@@ -21,7 +23,9 @@ for filename in os.listdir(directory):
           df = pd.read_csv(file_path, delimiter=',')
           # Création de la table dans la base de données
           df.to_sql(table_name, engine, if_exists='replace', index=False, schema='public')
-
+          print("======>>>>>  creation terminer  <<<<<======")
+          engine.dispose()
+          exit()
 # Fermeture de la connexion à la base de données
-print("======>>>>>  creation terminer  <<<<<======")
+print(" !!! file not found !!!")
 engine.dispose()
